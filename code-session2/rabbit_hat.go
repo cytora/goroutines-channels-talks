89e201c93cc228c705a6872f1a6cf0b5ae63191a
adelina-simion-cytora <adelina.simion@cytora.com>
2020-09-30 15:13:21 +0100

    add pointers printouts during pointer reassignment

--- code-session2/rabbit_hat.go
+++ code-session2/rabbit_hat.go
@@ -12,13 +12,17 @@ type RabbitHat struct {
 
 func hatTrickRabbit(hat RabbitHat) {
 	hat.contents = "a rabbit"
-	time.Sleep(2 * time.Second)
-	fmt.Printf("The hat contains %s inside function.\n", hat.contents)
+	time.Sleep(1 * time.Second)
+	fmt.Printf("The Rabbit hat is located at %p\n", &hat)
+	time.Sleep(1 * time.Second)
+	fmt.Printf("The Rabbit hat contains %s inside function.\n", hat.contents)
 }
 
 func (hat RabbitHat) trickFlowers() {
 	hat.contents = "flowers"
-	time.Sleep(2 * time.Second)
-	fmt.Printf("The hat contains %s inside function.\n", hat.contents)
+	time.Sleep(1 * time.Second)
+	fmt.Printf("The Flower hat is located at %p\n", &hat)
+	time.Sleep(1 * time.Second)
+	fmt.Printf("The Flower hat contains %s inside function.\n", hat.contents)
 }
 //END OMIT
--- code-session2/success/pointers_argument_passing.go
+++ code-session2/success/pointers_argument_passing.go
@@ -11,32 +11,37 @@ type PointerRabbitHat struct {
 
 func pointerHatTrickRabbit(hat *PointerRabbitHat) {
 	hat.contents = "a rabbit"
-	time.Sleep(2 * time.Second)
-	fmt.Printf("The hat contains %s inside function.\n", hat.contents)
+	time.Sleep(1 * time.Second)
+	fmt.Printf("The Rabbit hat is located at %p\n", hat)
+	time.Sleep(1 * time.Second)
+	fmt.Printf("The Rabbit hat contains %s inside function.\n", hat.contents)
 }
 
 func (hat *PointerRabbitHat) trickFlowers() {
 	hat.contents = "flowers"
-	time.Sleep(2 * time.Second)
-	fmt.Printf("The hat contains %s inside function.\n", hat.contents)
+	time.Sleep(1 * time.Second)
+	fmt.Printf("The Flower hat is located at %p\n", hat)
+	time.Sleep(1 * time.Second)
+	fmt.Printf("The Flower hat contains %s inside function.\n", hat.contents)
 }
 
 //START OMIT
 func main() {
 	hat := PointerRabbitHat{contents: "nothing"} //the hat begins by being empty
-	fmt.Printf("The magic show has started!\nThe hat contains %s.\n", hat.contents)
+	fmt.Println("The magic show has started!")
+	fmt.Printf("The original hat is located at %p\nThe hat contains %s.\n", &hat, hat.contents)
 
 	time.Sleep(2 * time.Second)
 	fmt.Println("Placing a rabbit in the hat.")
-	pointerHatTrickRabbit(&hat) //pass a pointer to the hat
+	pointerHatTrickRabbit(&hat) // try to place a rabbit in the hat
 	time.Sleep(2 * time.Second)
-	fmt.Printf("The hat contains %s after function.\n", hat.contents)
+	fmt.Printf("The original hat contains %s after function.\n", hat.contents)
 
 	time.Sleep(2 * time.Second)
 	fmt.Println("Placing flowers in the hat.")
 	hat.trickFlowers()
 	time.Sleep(2 * time.Second)
-	fmt.Printf("The hat contains %s after function.\n", hat.contents)
+	fmt.Printf("The original hat contains %s after function.\n", hat.contents)
 
 	time.Sleep(2 * time.Second)
 	fmt.Println("The magic show has ended!")
